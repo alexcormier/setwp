3666af4e08991f032698910fd017ffef3313b2b2
Alexandre Cormier <alexandre@cormier.io>
2015-04-02 10:26:05 -0400

    attempt at fixing zsh completion with homebrew

--- homebrew/setwp.rb
+++ homebrew/setwp.rb
@@ -2,20 +2,20 @@ require 'formula'
 
 class Setwp < Formula
     homepage 'https://github.com/alexandrecormier/setwp'
+    version '0.1.1-1'
 
-    ver = '0.1.1-1'
     if Hardware.is_64_bit?
-        url "https://github.com/alexandrecormier/setwp/releases/download/v#{ver}/setwp-amd64-v#{ver}.tar.gz"
+        url "https://github.com/alexandrecormier/setwp/releases/download/v#{version}/setwp-amd64-v#{version}.tar.gz"
         sha1 'fca3b7dd0682090eced04ee277159250d851a679'
     else
-        url "https://github.com/alexandrecormier/setwp/releases/download/v#{ver}/setwp-i386-v#{ver}.tar.gz"
+        url "https://github.com/alexandrecormier/setwp/releases/download/v#{version}/setwp-i386-v#{version}.tar.gz"
         sha1 'd4a4f42e2eb7c827247d4946d60bc3b822b10124'
     end
 
     def install
         bin.install 'setwp'
         bash_completion.install 'completion/setwp-completion.bash'
-        zsh_completion.install 'completion/setwp-completion.zsh'
+        zsh_completion.install 'completion/setwp-completion.zsh' => '_setwp'
     end
 
     test do
