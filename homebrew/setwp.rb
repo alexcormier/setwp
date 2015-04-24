e20431ecd35233074e203206ebbf2c208068793d
Alexandre Cormier <alexandre@cormier.io>
2015-04-24 00:35:29 -0400

    remove 32-bit stuff
    Yosemite does not even support the 32-bit kernel, so that was useless.
    Also make brew formula depend on Yosemite.

--- homebrew/setwp.rb
+++ homebrew/setwp.rb
@@ -4,13 +4,10 @@ class Setwp < Formula
     homepage 'https://github.com/alexandrecormier/setwp'
     version '0.1.1-1'
 
-    if Hardware.is_64_bit?
-        url "https://github.com/alexandrecormier/setwp/releases/download/v#{version}/setwp-amd64-v#{version}.tar.gz"
-        sha1 '9003f374a427724782b9b49776d639b7d6987e28'
-    else
-        url "https://github.com/alexandrecormier/setwp/releases/download/v#{version}/setwp-i386-v#{version}.tar.gz"
-        sha1 'aaa231ec22c565aef55b6313a074b4e458454527'
-    end
+    depends_on :macos => :yosemite
+
+    url "https://github.com/alexandrecormier/setwp/releases/download/v#{version}/setwp-amd64-v#{version}.tar.gz"
+    sha1 '9003f374a427724782b9b49776d639b7d6987e28'
 
     def install
         bin.install 'setwp'
--- script/package.sh
+++ script/package.sh
@@ -9,29 +9,23 @@ VERSION="$1"
 PROJECT_NAME='setwp'
 PROJECT_PATH="github.com/alexandrecormier/${PROJECT_NAME}"
 PROJECT_FULL_PATH="${GOPATH}/src/${PROJECT_PATH}"
-BINARY_PATH_32BIT="${GOPATH}/bin/darwin_386"
-BINARY_PATH_64BIT="${GOPATH}/bin"
-TARBALL_32BIT="${PROJECT_FULL_PATH}/releases/${PROJECT_NAME}-i386-v${VERSION}.tar.gz"
-TARBALL_64BIT="${PROJECT_FULL_PATH}/releases/${PROJECT_NAME}-amd64-v${VERSION}.tar.gz"
+BINARY_PATH="${GOPATH}/bin"
+TARBALL="${PROJECT_FULL_PATH}/releases/${PROJECT_NAME}-v${VERSION}.tar.gz"
 BREW_FORMULA="${PROJECT_FULL_PATH}/homebrew/${PROJECT_NAME}.rb"
 BREW_FORMULA_TEMPLATE="${BREW_FORMULA}.template"
 ARGS_FILE="${PROJECT_FULL_PATH}/args/args.go"
 
 # Build
 go install "${PROJECT_PATH}"
-GOARCH=386 CGO_ENABLED=1 go install "${PROJECT_PATH}"
 
 # Make tarballs
 mkdir -p releases
-tar -czf "${TARBALL_32BIT}" completion/* -C "${BINARY_PATH_32BIT}" "${PROJECT_NAME}"
-tar -czf "${TARBALL_64BIT}" completion/* -C "${BINARY_PATH_64BIT}" "${PROJECT_NAME}"
+tar -czf "${TARBALL}" completion/* -C "${BINARY_PATH}" "${PROJECT_NAME}"
 
 # Update homebrew formula
-SHA256_32BIT=`openssl dgst -sha256 ${TARBALL_32BIT} | xxd -ps | tr -d '\n'`
-SHA256_64BIT=`openssl dgst -sha256 ${TARBALL_64BIT} | xxd -ps | tr -d '\n'`
+SHA256_HASH=`openssl dgst -sha256 ${TARBALL} | xxd -ps | tr -d '\n'`
 sed -e "s/<VERSION>/${VERSION}/g" \
-    -e "s/<SHA256_32BIT>/${SHA256_32BIT}/" \
-    -e "s/<SHA256_64BIT>/${SHA256_64BIT}/" \
+    -e "s/<SHA256_HASH>/${SHA256_HASH}/" \
     "${BREW_FORMULA_TEMPLATE}" > "${BREW_FORMULA}"
 
 # Bump version
