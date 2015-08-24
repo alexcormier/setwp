95f1b8813c0b0cb05bff47d7bd56b75fbd982e56
Alexandre Cormier <alexandre@cormier.io>
2015-08-24 00:13:44 -0400

    update homebrew formula for the last time

--- script/package.sh
+++ script/package.sh
@@ -12,8 +12,6 @@ PROJECT_PATH="github.com/alexandrecormier/${PROJECT_NAME}"
 PROJECT_FULL_PATH="${GOPATH}/src/${PROJECT_PATH}"
 BINARY_PATH="${GOPATH}/bin"
 TARBALL="${PROJECT_FULL_PATH}/releases/${PROJECT_NAME}-v${VERSION}.tar.gz"
-BREW_FORMULA="${PROJECT_FULL_PATH}/homebrew/${PROJECT_NAME}.rb"
-BREW_FORMULA_TEMPLATE="${BREW_FORMULA}.template"
 ARGS_FILE="${PROJECT_FULL_PATH}/args/args.go"
 
 # Bump version
@@ -22,12 +20,6 @@ sed -E -i '' "s/version [0-9\.]+/version ${SHORT_VERSION}/" "${ARGS_FILE}"
 # Build
 go install "${PROJECT_PATH}"
 
-# Make tarballs
+# Make tarball
 mkdir -p releases
 tar -czf "${TARBALL}" completion/* -C "${BINARY_PATH}" "${PROJECT_NAME}"
-
-# Update homebrew formula
-SHA256_HASH=`openssl dgst -sha256 ${TARBALL} | cut -f2 -d" "`
-sed -e "s/<VERSION>/${VERSION}/g" \
-    -e "s/<SHA256_HASH>/${SHA256_HASH}/" \
-    "${BREW_FORMULA_TEMPLATE}" > "${BREW_FORMULA}"
